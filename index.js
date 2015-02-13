e6c3f6bf70b329fd32c477f59d317820fad55498
Dmitry Matveev <dmitrymatveev@yahoo.co.nz>
2015-02-13 17:00:51 +1300

    misc project build related stuff

--- index.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    Model : require( path.resolve(__dirname, './lib/Model.js') ),
-    DataType : require( path.resolve(__dirname, './lib/DataType.js') ),
-    Transport : require( path.resolve(__dirname, './lib/Transport.js') ),
-    ObjectPool : require( path.resolve(__dirname, './lib/ObjectPool.js') )
-};
